b165fd720507c4a7959cbe024527a34c6e4e1201
Eric Li <li.eric00@gmail.com>
2017-09-22 18:55:41 -0400

    Update JavaReview3_1.java
    Use Math.exp instead of Math.pow

--- src/JavaReview3/JavaReview3_1.java
+++ src/JavaReview3/JavaReview3_1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class JavaReview3_1 
 {
 	public static double naturalPower(int base, int power) {
-		return Math.pow(Math.E, power * Math.log(base));
+		return Math.exp(power * Math.log(base));
 	}
 	public static void main(String[] args)
 	{
